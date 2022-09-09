7b74d2f77dd03a6a4473c4b1484a40997e3244e2
Rob <rob.rudary@gmail.com>
2022-09-09 16:04:43 -0400

    Bug fix

--- ref_search.py
+++ ref_search.py
@@ -16,7 +16,7 @@ def prices(ref):
             options = Options()
             options.headless = True
             options.add_argument("--window-size=1920,1200")
-            DRIVER_PATH = os.environ['cd_path']
+            DRIVER_PATH = os.environ.get('cd_path')
             driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
             driver.get(url)
             element = driver.find_elements(By.ID, "wt-watches")[0].get_attribute("innerHTML")
