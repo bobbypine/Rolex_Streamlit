025b5af1a8fa72e8d0e98cb39d7c1baead938f90
Rob <rob.rudary@gmail.com>
2022-09-11 11:21:12 -0400

    Adding function to create index chart

---
+++ index.py
@@ -0,0 +1,8 @@
+def index_chart(data):
+    # df = data[ref].copy()
+    df = data
+    df.dropna(axis=0, inplace=True)
+    columns = [x for x in df.columns]
+    for x in columns:
+        df[x] = (df[x]/df.iloc[0,columns.index(x)])
+    return df
